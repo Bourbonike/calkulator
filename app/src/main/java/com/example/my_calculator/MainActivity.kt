fba162adc4112a2792ed1cc287993bc8f4226822
Bourbonike <100319658+Bourbonike@users.noreply.github.com>
2023-06-25 23:45:11 +0300

    1.view_binding (#1)
    * 1.view_binding
    2.lanscape_layout
    
    * fix view_binding

--- app/src/main/java/com/example/my_calculator/MainActivity.kt
+++ app/src/main/java/com/example/my_calculator/MainActivity.kt
@@ -4,29 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import com.example.my_calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
+        binding.btn0.setOnClickListener { setTextFields("0") }
         //1.2.3.4.5.6.7.8.9
-        findViewById<TextView>(R.id.btn_0).setOnClickListener { setTextFields("0") }
-        findViewById<TextView>(R.id.btn_1).setOnClickListener { setTextFields("1") }
-        findViewById<TextView>(R.id.btn_2).setOnClickListener { setTextFields("2") }
-        findViewById<TextView>(R.id.btn_3).setOnClickListener { setTextFields("3") }
-        findViewById<TextView>(R.id.btn_4).setOnClickListener { setTextFields("4") }
-        findViewById<TextView>(R.id.btn_5).setOnClickListener { setTextFields("5") }
-        findViewById<TextView>(R.id.btn_6).setOnClickListener { setTextFields("6") }
-        findViewById<TextView>(R.id.btn_7).setOnClickListener { setTextFields("7") }
-        findViewById<TextView>(R.id.btn_8).setOnClickListener { setTextFields("8") }
-        findViewById<TextView>(R.id.btn_9).setOnClickListener { setTextFields("9") }
+        //findViewById<TextView>(R.id.btn_0).setOnClickListener { setTextFields("0") }
+        binding.btn1.setOnClickListener { setTextFields("1") }
+        binding.btn2.setOnClickListener { setTextFields("2") }
+        binding.btn3.setOnClickListener { setTextFields("3") }
+        binding.btn4.setOnClickListener { setTextFields("4") }
+        binding.btn5.setOnClickListener { setTextFields("5") }
+        binding.btn6.setOnClickListener { setTextFields("6") }
+        binding.btn7.setOnClickListener { setTextFields("7") }
+        binding.btn8.setOnClickListener { setTextFields("8") }
+        binding.btn9.setOnClickListener { setTextFields("9") }
         //plus_btn
-        findViewById<TextView>(R.id.minus_btn).setOnClickListener { setTextFields("-") }
-        findViewById<TextView>(R.id.plus_btn).setOnClickListener { setTextFields("+") }
-        findViewById<TextView>(R.id.mult_btn).setOnClickListener { setTextFields("*") }
-        findViewById<TextView>(R.id.disivion_btn).setOnClickListener { setTextFields("/") }
+        binding.minusBtn.setOnClickListener { setTextFields("-") }
+        binding.plusBtn.setOnClickListener { setTextFields("+") }
+        binding.multBtn.setOnClickListener { setTextFields("*") }
+        binding.disivionBtn.setOnClickListener { setTextFields("/") }
+        binding.square?.setOnClickListener { setTextFields("√") }
         //
         findViewById<TextView>(R.id.bracket_open_btn).setOnClickListener { setTextFields("(") }
         findViewById<TextView>(R.id.bracket_close_btn).setOnClickListener { setTextFields(")") }
@@ -68,7 +75,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString("KEY",findViewById<TextView>(R.id.result_text).text.toString())
+        outState.putString("KEY", findViewById<TextView>(R.id.result_text).text.toString())
         super.onSaveInstanceState(outState)
     }
 
